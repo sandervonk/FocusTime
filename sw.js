19742f2890f76408f3b4e444a25dc92b717f2def
Sander Vonk <sander.c.vonk@gmail.com>
2022-10-08 12:00:35 -0700

    add commonly used icons to cashe

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open("FocusTime-[-version-number-]").then(function (cache) {
-      return cache.addAll(["./js/util.js", "./js/auth.js", "./js/index.js", "./js/signup.js", "./js/app.js", "./index.html", "./signup.html", "./app/index.html", "./img/icon/favicon.png", "./img/icon/favicon.svg", "./img/icon/favicon-monochrome.png", "./img/icon/favicon-monochrome.svg"]);
+      return cache.addAll(["./js/util.js", "./js/auth.js", "./js/index.js", "./js/signup.js", "./js/app.js", "./index.html", "./signup.html", "./app/index.html", "./img/icon/favicon.png", "./img/icon/favicon.svg", "./img/icon/favicon-monochrome.png", "./img/icon/favicon-monochrome.svg", "./img/icon/tasks/date-icon.svg", "./img/icon/tasks/clock-icon.svg", , "./img/icon/tasks/date-icon.png", "./img/icon/tasks/clock-icon.png"]);
     })
   );
 });
