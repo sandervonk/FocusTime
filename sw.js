f2b4071bb858943a0a53d24c9d5d90d27e41ad63
sandervonk <sandervonk@users.noreply.github.com>
2023-02-28 19:34:38 +0000

    Sync & minify '89567b7d4fc39659e15ad67054a79e73175a261e' from main

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open("FocusTime-[-version-number-]").then(function (cache) {
+    caches.open("FocusTime-89567b7d4fc39659e15ad67054a79e73175a261e").then(function (cache) {
       return cache.addAll(["./js/util.min.js", "./js/auth.min.js", "./js/index.min.js", "./js/signup.min.js", "./js/app.min.js", "./index.html", "./signup.html", "./app/index.html", "./img/icon/favicon.png", "./img/icon/favicon.svg", "./img/icon/favicon-monochrome.png", "./img/icon/favicon-monochrome.svg", "./img/icon/tasks/date-icon.svg", "./img/icon/tasks/clock-icon.svg", "./img/icon/tasks/date-icon.png", "./img/icon/tasks/clock-icon.png"]);
     })
   );
